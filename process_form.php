d7c59481168bfe91a0f1830db67bb92a5e727c8e
amincitravels <123930954+amincitravels@users.noreply.github.com>
2023-12-05 17:33:50 +0300

    Create process_form.php

---
+++ process_form.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    // Replace 'bassil@amincitravels.com' with the actual email address where you want to receive form submissions
+    $to = "bassil@amincitravels.com";
+    $subject = "New Form Submission from Aminci Travels";
+    $headers = "From: $email";
+
+    // Compose the email message
+    $email_message = "Name: $name\n";
+    $email_message .= "Email: $email\n";
+    $email_message .= "Message:\n$message";
+
+    // Send the email
+    mail($to, $subject, $email_message, $headers);
+
+    // Redirect back to the thank you page or your homepage
+    header("Location: thank_you.html");
+    exit();
+}
+?>
